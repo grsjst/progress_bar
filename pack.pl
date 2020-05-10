1b285976d82bcbaee3250418783c957981d5b012
Jozef Geurts <jpt.geurts@gmail.com>
2020-05-10 12:04:42 +0200

    added pack.pl

---
+++ pack.pl
@@ -0,0 +1,5 @@
+name(progress_bar).
+version('0.0.5').
+title('Progress-bar and spinner (text-based)').
+author('Jozef Geurts', 'http://joostgeurts.com/').
+home('https://github.com/grsjst/progress_bar').
